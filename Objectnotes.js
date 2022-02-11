087a9a571db9c145dd65ad65898741b61265f0ef
nic0mp <nico.mp215@gmail.com>
2022-02-10 21:06:11 -0600

    dot notation

--- Objectnotes.js
+++ Objectnotes.js
@@ -3,3 +3,15 @@ const fasterShip = {
     'Fuel Type': 'Turbo Fuel',
     color: 'silver'
   };
+
+//   Dot Notation
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  const crewCount = spaceship.numCrew;
+  const planetArray = spaceship.flightPath;
