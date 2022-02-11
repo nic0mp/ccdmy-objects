dd6dae375c24a5b7b2f17d3c80fe76cc1d032cbb
nic0mp <nico.mp215@gmail.com>
2022-02-11 14:19:48 -0600

    property assignment

--- Objectnotes.js
+++ Objectnotes.js
@@ -29,3 +29,18 @@ let spaceship = {
   // Invoke
   let isActive = spaceship['Active Mission']
   console.log(spaceship['Active Mission'])
+
+//   Property Assignment
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+// Reassign the color property of the spaceship object to have a value of 'glorious gold'
+spaceship.color = 'glorious gold';
+//  add a numEngines property with a numeric value between 1 and 10 to the spaceship object. 
+spaceship.numEngines = 7;
+// Use the delete operator to remove the 'Secret Mission' property from the spaceship object.
+delete spaceship['Secret Mission'];
