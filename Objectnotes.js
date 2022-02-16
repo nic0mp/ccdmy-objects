0b57118226cb05b941183673a61c110d4903ad36
nic0mp <nico.mp215@gmail.com>
2022-02-16 13:10:35 -0600

    advanced objects

--- Objectnotes.js
+++ Objectnotes.js
@@ -171,32 +171,3 @@ console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].de
 // Clementine: Physics
 // Shauna: Conservation Science
 
-// THIS keyword
-const robot = {
-  model: '1E78V2',
-  energyLevel: 100,
-  provideInfo(){return`I am ${this.model} and my current energy level is ${this.energyLevel}. `}
-};
-console.log(robot.provideInfo())
-// OUTPUT: I am 1E78V2 and my current energy level is 100. 
-
-// Arrow functions and this
-
-const robot = {
-  energyLevel: 100,
-  checkEnergy: () => {
-    console.log(`Energy is currently at ${this.energyLevel}%.`)
-  }
-}
-
-robot.checkEnergy(); 
-// Avoid arrow functions with this key ^^^
-
-const robot = {
-  energyLevel: 100,
-  checkEnergy() {
-    console.log(`Energy is currently at ${this.energyLevel}%.`)
-  }
-}
-
-robot.checkEnergy();
