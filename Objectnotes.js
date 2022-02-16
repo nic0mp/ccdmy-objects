f246b6ea4a39ee210eb88a11441bf82da9f13f86
nic0mp <nico.mp215@gmail.com>
2022-02-15 21:29:59 -0600

    this key

--- Objectnotes.js
+++ Objectnotes.js
@@ -170,3 +170,12 @@ console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].de
 // Dan: Aerospace Engineering
 // Clementine: Physics
 // Shauna: Conservation Science
+
+// THIS keyword
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo(){return`I am ${this.model} and my current energy level is ${this.energyLevel}. `}
+};
+console.log(robot.provideInfo())
+// OUTPUT: I am 1E78V2 and my current energy level is 100. 
