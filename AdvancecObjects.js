0cc80b789892a55d81265edda82aead642d290bf
nic0mp <nico.mp215@gmail.com>
2022-02-16 14:18:26 -0600

    getter

--- AdvancecObjects.js
+++ AdvancecObjects.js
@@ -27,3 +27,20 @@ const robot = {
   }
   
   robot.checkEnergy();
+
+//   Getters
+const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel(){
+      if(typeof this._energyLevel === 'number'){
+        return `My current energy level is ${this._energyLevel}`
+      } else {
+        return `System malfunction: cannot retrieve energy level`
+      }
+    }
+  };
+  console.log(robot.energyLevel);
+//   output My current energy level is 100
+
+  
