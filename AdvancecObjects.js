9d5376dcb40531f26e60eebb56c1cb97e42b18d2
nic0mp <nico.mp215@gmail.com>
2022-02-16 21:18:14 -0600

    setters

--- AdvancecObjects.js
+++ AdvancecObjects.js
@@ -43,4 +43,28 @@ const robot = {
   console.log(robot.energyLevel);
 //   output My current energy level is 100
 
+// Setters
+const robot = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    _numOfSensors: 15,
+    get numOfSensors(){
+      if(typeof this._numOfSensors === 'number'){
+        return this._numOfSensors;
+      } else {
+        return 'Sensors are currently down.'
+      }
+    },
+    set numOfSensors(num){
+      if(typeof num === 'number' && num >= 0){
+        this._numOfSensors = num
+      } else {
+        console.log('Pass in a number that is greater than or equal to 0')
+      }
+    }
+  };
+  robot.numOfSensors = 100;
+  console.log(robot.numOfSensors)
+// output: 100
+  
   
