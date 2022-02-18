4978e51a323a199fdcec39bce0e87446dbd1ee42
nic0mp <nico.mp215@gmail.com>
2022-02-18 14:30:10 -0600

    Meal maker

---
+++ MealMaker.js
@@ -0,0 +1,32 @@
+const menu ={
+    _courses: { 
+      appetizers : [],
+      mains : [],
+      desserts : [],
+    },
+    get _courses(){
+      return {
+        appetizers: this.appetizers,
+        mains: this.mains,
+        desserts: this.desserts
+      }
+    },
+    get appetizers(){
+  
+    },
+    set appetizers(){
+  
+    },
+    get mains(){
+  
+    },
+    set mains(){
+  
+    },
+    get desserts(){
+  
+    },
+    set desserts(){
+  
+    }
+  };
