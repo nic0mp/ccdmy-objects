1820a11bbba746eadfd730725ffd2599304ad154
nic0mp <nico.mp215@gmail.com>
2022-02-18 22:16:23 -0600

    get random dish from course

--- MealMaker.js
+++ MealMaker.js
@@ -14,19 +14,33 @@ const menu ={
     get appetizers(){
   
     },
-    set appetizers(){
+    set appetizers(appetizersIn){
   
     },
     get mains(){
   
     },
-    set mains(){
+    set mains(mainsIn){
   
     },
     get desserts(){
   
     },
-    set desserts(){
+    set desserts(dessertsIn){
   
-    }
+    },
+    addDishToCourse(courseName, dishName, dishPrice){
+      const dish = {
+        name: dishName,
+        price: dishPrice,
+      };
+      this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse(courseName){
+      const dishes = this._courses[courseName];
+      const randomIndex = Math.floor(Math.random*dishes.length);
+      return dishes[randomIndex];
+    },
+    
+    
   };
