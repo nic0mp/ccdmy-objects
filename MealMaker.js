108b87ac97dce0e623f2650c7eb247c7850cd07a
nic0mp <nico.mp215@gmail.com>
2022-02-19 21:42:05 -0600

    meal maker

--- MealMaker.js
+++ MealMaker.js
@@ -12,10 +12,10 @@ const menu ={
       }
     },
     get appetizers(){
-  
+      return this._courses.appetizers
     },
     set appetizers(appetizersIn){
-  
+      this._courses.appetizers = appetizersIn
     },
     get mains(){
   
@@ -30,20 +30,27 @@ const menu ={
   
     },
     addDishToCourse(courseName, dishName, dishPrice){
-        const dish = {
-            name: dishName,
-            price: dishPrice,
-        };
-        this._courses[courseName].push(dish);
+      const dish = {
+        name: dishName,
+        price: dishPrice,
+      };
+      this._courses[courseName].push(dish);
     },
     getRandomDishFromCourse(courseName){
-        const dishes = this._courses[courseName];
-        const randomIndex = Math.floor(Math.random*dishes.length);
-        return dishes[randomIndex];
+      const dishes = this._courses[courseName];
+      const randomIndex = Math.floor(Math.random*dishes.length);
+      return dishes[randomIndex];
     },
     generateRandomMeal(){
-        const appetizer = this.getRandomDishFromCourse('appetizers'),
-
+      const appetizer = this.getRandomDishFromCourse('appetizers'),
+      const dessert = this.getRandomDishFromCourse('desserts'),
+      const totalPrice = appetizer.price + main.price + dessert.price;
+  
+       return `Your meal is ${appetizer.name}, ${main.name}, ... The    price is $${totalPrice}.`;
     }
     
   };
+  
+  menu.addDishToCourse('appetizers', 'stuffed mushrooms', 12.00)
+  menu.addDishToCourse('appetizers', 'chicken satay', 7.70)
+  menu.addDishToCourse('appetizers', 'salad', 5.50)
